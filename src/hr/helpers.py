98e9cc57b92c0d0af7d39d9f8494b0a8a2adb074
Ricardo Maximo Jr <ricardomaximojr@gmail.com>
2019-08-12 01:51:52 +0000

    final commit

--- src/hr/helpers.py
+++ src/hr/helpers.py
@@ -1,9 +1,4 @@
 import pwd
 
 def user_names():
-    return [ user.pw_name for user in pwd.getpwall()
-<<<<<<< HEAD
-            if user.pw_uid >= 1000 and 'home' in user.pw_dir]
-=======
-            if user.pw_uid >= 1000 and 'home' in user.pw_dir]
->>>>>>> 7948095d8bc8122ef2ba4db36af91f2f7bb294b3
+    return [ user.pw_name for user in pwd.getpwall() if user.pw_uid >= 1000 and 'home' in user.pw_dir]
